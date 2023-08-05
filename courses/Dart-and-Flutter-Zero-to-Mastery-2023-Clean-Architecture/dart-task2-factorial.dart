d4331e344b4c20dff33430b476731489d7e5f061
Artur Assis Alves <arturassiscomp@gmail.com>
2023-08-05 10:26:04 -0300

    Add final keyword to the variables that will not change after the first assignment.

--- courses/Dart-and-Flutter-Zero-to-Mastery-2023-Clean-Architecture/dart-task2-factorial.dart
+++ courses/Dart-and-Flutter-Zero-to-Mastery-2023-Clean-Architecture/dart-task2-factorial.dart
@@ -4,19 +4,19 @@ import 'dart:io';
 void main(){
 
   stdout.write('Enter an integer greater or equal to 0: ');
-  String? userInput = stdin.readLineSync();
+  final String? userInput = stdin.readLineSync();
 
   if (userInput == null){
     print('No input received.');
   }else{
-    int? userInputNumber = int.tryParse(userInput);
+    final int? userInputNumber = int.tryParse(userInput);
     if (userInputNumber == null){
       print('Invalid input ($userInput). It should be an integer');
     }else{
       if(userInputNumber < 0){
         print('Invalid input ($userInputNumber). It must be greater or equal to 0.');
       }else{
-        int factorialResult = factorial(userInputNumber);
+        final int factorialResult = factorial(userInputNumber);
         print('The result is $factorialResult');
       }
     }
