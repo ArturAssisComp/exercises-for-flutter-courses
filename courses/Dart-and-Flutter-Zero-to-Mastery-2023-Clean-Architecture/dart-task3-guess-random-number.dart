e0d6b7073bc93e577bbdab71c3569c7041d7871f
Artur Assis Alves <arturassiscomp@gmail.com>
2023-08-05 20:33:44 -0300

    Add hints to the user

--- courses/Dart-and-Flutter-Zero-to-Mastery-2023-Clean-Architecture/dart-task3-guess-random-number.dart
+++ courses/Dart-and-Flutter-Zero-to-Mastery-2023-Clean-Architecture/dart-task3-guess-random-number.dart
@@ -1,11 +1,19 @@
 import 'dart:math';
 import 'dart:io';
 
+const String _kTooLow = 'Too low';
+const String _kTooHigh = 'Too high';
+const String _kLittleLow = 'A little low';
+const String _kLittleHigh = 'A little high';
+const int _kBigThreshold = 50;
+const int _kSmallThreshold = 50;
+
 void main(){
 
   final int randomNumber = Random().nextInt(100) + 1;
   print('A random number between 1 and 100 (inclusive) was generated. Try to guess it!');
   int i = 0;
+  int diff = 0;
   String? userInput;
   int? userGuess;
   while(true){
@@ -16,6 +24,18 @@ void main(){
       print('You found the correct answer: $userGuess');
       print('Correct guess after ${i + 1} tries.');
       return;
+    }
+    if(userGuess != null){
+      diff = userGuess - randomNumber;
+      if (diff >= _kBigThreshold){
+        print(_kTooHigh);
+      }else if (diff >= 0){
+        print(_kLittleHigh);
+      }else if (diff >= -_kSmallThreshold){
+        print(_kLittleLow);
+      }else {
+        print(_kTooLow);
+      }
     }
     i++;
   }
